daf7f763a7e7501dfd3306b2275c47b8f66f72af
justinzl1 <justinzlee2016@gmail.com>
2025-10-16 17:00:21 -0700

    adding times back and fixing text wrapping issue

--- components/agenda.tsx
+++ components/agenda.tsx
@@ -46,32 +46,21 @@ export function AgendaSection() {
 
           <div className="relative">
 
-            <div className="bg-[var(--color-light-purple)] rounded-xl xl:rounded-2xl p-4 sm:p-6 xl:p-10 2xl:p-10 backdrop-blur-sm font-lucidity text-[var(--color-blue)]">
-              <div className="text-[clamp(1rem,1vw+0.25rem,2rem)] hidden lg:block lg:flex gap-4 sm:gap-6 xl:gap-20 2xl:gap-12 px-5 lg:px-10 pb-10 lg:pb-70 2xl:pb-120">
+            <div className="bg-[var(--color-light-purple)] rounded-xl xl:rounded-2xl p-4 sm:p-6 xl:p-10 2xl:p-10 pr-32 xl:pr-48 2xl:pr-64 backdrop-blur-sm font-lucidity text-[var(--color-blue)]">
+              <div className="hidden lg:flex flex-col text-[clamp(1rem,1vw+0.25rem,2rem)] gap-3 xl:gap-4 px-5 lg:px-10 pb-10 -ml-6 xl:-ml-10 2xl:-ml-12">
                 {/* Left column - Times */}
-                <div className="hidden flex flex-col space-y-2 sm:space-y-3 xl:space-y-4 2xl:space-y-5">
-                  {scheduleItems.map((item, index) => (
-                    <span
-                      key={index}
-                      className="font-bold py-1 sm:py-2"
-                    >
-                      {item.time}
-                    </span>
-                  ))}
+              {scheduleItems.map((item, index) => (
+                <div key={index} className="flex flex-row items-start gap-8 xl:gap-16 2xl:gap-20">
+                  <span className="font-bold w-[10rem] shrink-0 text-right">
+                    {item.time}
+                  </span>
+                  <span className="font-bold flex-1 text-left break-words">
+                    {item.title}
+                  </span>
                 </div>
+              ))}
+            </div>
 
-                {/* Right column - Titles */}
-                <div className="flex flex-col space-y-2 sm:space-y-3 xl:space-y-4 2xl:space-y-5 flex-1">
-                  {scheduleItems.map((item, index) => (
-                    <span
-                      key={index}
-                      className="font-bold py-1 sm:py-2"
-                    >
-                      {item.title}
-                    </span>
-                  ))}
-                </div>
-              </div>
               
               {/* mobile layout times */}
               <div className="lg:hidden space-y-2 sm:space-y-3 xl:space-y-4 2xl:space-y-5">
@@ -93,6 +82,9 @@ export function AgendaSection() {
           </div>
 
           {/* Illustration placeholders */}
+          {/* Spacer to push down illustrations */}
+          <div className="hidden lg:block h-40 xl:h-64 2xl:h-80 bg-[var(--color-light-purple)] rounded-xl xl:rounded-2xl p-4 sm:p-6 xl:p-10 2xl:p-10 pr-32 xl:pr-48 2xl:pr-64 backdrop-blur-sm"></div>
+
           <div className="relative mt-8 hidden lg:block">
             {/* Left figure - person sitting/reclining */}
             <div className="absolute bottom-70 2xl:bottom-0 left-0 w-48 xl:w-70 2xl:w-80 h-60 xl:h-80 2xl:h-96 rounded-lg flex items-center justify-center">
@@ -117,7 +109,7 @@ export function AgendaSection() {
                     alt="Margaret Ham Portrait"
                     width={450}
                     height={450}
-                    className="hidden md:block md:relative right-160 2xl:right-210 -bottom-100 2xl:-bottom-10 w-380 2xl:w-500 object-contain max-w-none h-pointer-events-none"
+                    className="hidden md:block md:relative right-157 2xl:right-210 -bottom-100 2xl:-bottom-10 w-380 2xl:w-500 object-contain max-w-none h-pointer-events-none"
                   />
                 </div>
             </div>
